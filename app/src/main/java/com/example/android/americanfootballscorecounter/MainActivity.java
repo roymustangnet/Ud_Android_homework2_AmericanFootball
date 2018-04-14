9e2f02c8de5933d2fe8a41e2432d13aa992d1101
Roy Mustang <roy.mustang.net@gmail.com>
2018-04-15 00:25:06 +0800

    add process code

--- app/src/main/java/com/example/android/americanfootballscorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/americanfootballscorecounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.americanfootballscorecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void ResetScore(View view) {
+        teamAScore = 0;
+        teamBScore = 0;
+        display();
+    }
+
+    public void add_touchdown_A(View view) {
+        teamAScore += 6;
+        display();
+    }
+
+    public void add_fieldgoal_A(View view) {
+        teamAScore += 3;
+        display();
+    }
+
+    public void add_safety_A(View view) {
+        teamAScore += 2;
+        display();
+    }
+
+    public void add_extrapoint_A(View view) {
+        teamAScore += 1;
+        display();
+    }
+
+    public void add_touchdown_B(View view) {
+        teamBScore += 6;
+        display();
+    }
+
+    public void add_fieldgoal_B(View view) {
+        teamBScore += 3;
+        display();
+    }
+
+    public void add_safety_B(View view) {
+        teamBScore += 2;
+        display();
+    }
+
+    public void add_extrapoint_B(View view) {
+        teamBScore += 1;
+        display();
+    }
+
+
+    public void display() {
+        TextView scoreViewA = (TextView) findViewById(R.id.scoreA);
+        TextView scoreViewB = (TextView) findViewById(R.id.scoreB);
+        scoreViewA.setText(String.valueOf(teamAScore));
+        scoreViewB.setText(String.valueOf(teamBScore));
+    }
+
+
 }
